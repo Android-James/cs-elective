d2adc81e9c212f809778afaa9da1c95ba85adbff
James Luis Bediot Blanco <blancojames88@gmail.com>
2022-05-16 03:04:14 +0800

    modified shop page, added classes such as price and quantity, added remove function in cart, not done

--- cart-page/script.js
+++ cart-page/script.js
@@ -1,1 +1,19 @@
-console.log("Here")
+var removeCartItemButtons = document.getElementsByClassName('remove-btn')
+console.log(removeCartItemButtons)
+for (var i=0; i<removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event){
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.parentElement.remove()
+        updateCartTotal()
+    })
+}
+
+function updateCartTotal(){
+    var carItemContainer = document.getElementsByClassName('pro-container')[0]
+    var cartRows = carItemContainer.getElementsByClassName('pro')
+    for (var i=0; i<cartRows; i++){
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('price')[0]
+    }
+}
